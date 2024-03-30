d279c53eff61229d32c92eca68e0b5260e274f20
StalicJi <a7330831@gmail.com>
2024-03-31 01:36:38 +0800

    error for fetch

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,3 +1,18 @@
-export default function WebSearchPage() {
-  return <div>WebSearchPage</div>;
+export default async function WebSearchPage() {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&
+    cx=${process.env.CONTEXT_KEY}&q=lectures`
+  );
+  const data = await response.json();
+  console.log(data);
+  const results = data.items;
+  // console.log(results);
+  return (
+    <div>
+      {results &&
+        results.map((result) => {
+          <h1>{result.title}</h1>;
+        })}
+    </div>
+  );
 }
